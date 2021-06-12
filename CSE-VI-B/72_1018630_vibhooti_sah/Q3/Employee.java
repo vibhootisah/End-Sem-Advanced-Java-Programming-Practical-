4c28c108625a5122ed9e13a899ca8e85c5c1e944
vibhootisah <85724006+vibhootisah@users.noreply.github.com>
2021-06-12 10:33:25 +0530

    Add files via upload

---
+++ CSE-VI-B/72_1018630_vibhooti_sah/Q3/Employee.java
@@ -0,0 +1,36 @@
+ package Practical12;
+
+import com.opensymphony.xwork2.ActionSupport;
+@SuppressWarnings("serial")
+
+public class Employee extends ActionSupport{
+	 private String name;
+	 private int age;
+	 
+	 public String execute() {
+		 return "success";
+	 }
+	 public void validate() {
+		 if(name==null || name.trim().equals("")) {
+			 addFieldError("name","The name is required");
+		 }
+		 if(age<28 || age>65)
+		 {
+			 addFieldError("age","Age must be in between 28 and 65");
+		 }
+	 }
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	 
+	 
+}
